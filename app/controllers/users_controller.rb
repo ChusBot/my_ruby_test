d2242c3e23c7ae3593e24de018340b8fe1a6afa7
Michael Kovtunov <kovtunov.m.s@gmail.com>
2022-06-26 17:53:04 +0300

    feat: full CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new()
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to @user
@@ -20,4 +20,30 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :chat_id)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :chat_id, presence: true
+    validates :name, presence: true, length: { minimum: 5 }
 end
